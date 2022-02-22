548ac7457b4745797f57d7251d66c46b2146d226
sj3kenne <sam.kennedy@live.ca>
2022-02-22 14:58:57 -0500

    minor count changes

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 
 app.get('/', (req, res) => {
     const count = readFileSync('./count.txt', 'utf-8');
-    console.log('count', count)
+    console.log('count ', count);
 
-    const newCount = parseInt(count) + 1
+    const newCount = parseInt(count) + 1;
 
     writeFileSync('./count.txt', newCount);
 
