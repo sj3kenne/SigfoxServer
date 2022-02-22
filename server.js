9a4f1eadb79928804aef7a1b45a40dc39d664548
sj3kenne <sam.kennedy@live.ca>
2022-02-22 13:00:36 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,30 @@
+const { readFileSync, writeFileSync } = require('fs');
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    const count = readFileSync('./count.txt', 'utf-8');
+    console.log('count', count)
+
+    const newCount = parseInt(count) + 1
+
+    writeFileSync('./count.txt', newCount);
+
+    res.send(`
+            <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width,initial-scale=1">
+                <title>RPi Sigfox Server</title>
+            </head>
+            <body>
+                <Welcome to my website!</h1>
+                <p>This page has been viewed ${newCount} times!</p>
+            </body>
+        </html>
+    `)
+})
+
+app.listen(5000, () => console.log('http://localhost:5000/'));
