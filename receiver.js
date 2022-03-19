81a6890df943acd10bf9e7026fe90990c70291a3
sj3kenne <sam.kennedy@live.ca>
2022-03-18 22:12:44 -0400

    Update receiver.js

--- receiver.js
+++ receiver.js
@@ -4,7 +4,7 @@ var express = require('express');
 const bodyParser = require("body-parser");
 
 //const Promise = require('bluebird')
-const AppDAO = require('./dao')
+const AppDAO = require('./tools/dao')
 const MessageRepository = require('./tools/message_repository')
 
 //var db = require("./tools/database.js")
